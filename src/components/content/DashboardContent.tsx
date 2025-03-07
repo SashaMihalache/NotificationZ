f46e666b2ee00c4627d6a0e23616cb5fb26f9535
Sasha Mihalache <sasha.mihalache@gmail.com>
2025-03-07 22:22:07 +0200

    feat: modals and delete

--- src/components/content/DashboardContent.tsx
+++ src/components/content/DashboardContent.tsx
@@ -2,11 +2,18 @@
 
 import { client } from "@/app/lib/client"
 import { LoadingSpinner } from "../LoadingSpinner"
-import { useQuery } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { format, formatDistance, formatDistanceToNow } from "date-fns"
-import { BarChart2, Clock, Database } from "lucide-react"
+import { ArrowRight, BarChart2, Clock, Database, Trash2 } from "lucide-react"
+import Link from "next/link"
+import { Button, buttonVariants } from "../ui/button"
+import { useState } from "react"
+import { Modal } from "../ui/modal"
 
 export const DashboardContent = () => {
+  const [deletingCategory, setDeletingCategory] = useState<string | null>(null)
+  const queryClient = useQueryClient()
+
   const { data: categories, isPending: isEventCategoriesLoading } = useQuery({
     queryKey: ["user-event-categories"],
     queryFn: async () => {
@@ -14,6 +21,16 @@ export const DashboardContent = () => {
 
       const { categories } = await res.json()
       return categories
+    },
+  })
+
+  const { mutate: deleteCategory, isPending: isDeleteLoading } = useMutation({
+    mutationFn: async (name: string) => {
+      await client.category.deleteCategory.$post({ name })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["user-event-categories"] })
+      setDeletingCategory(null)
     },
   })
 
@@ -34,62 +51,128 @@ export const DashboardContent = () => {
   }
 
   return (
-    <ul className="grid max-w-6xl grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
-      {categories.map((category) => (
-        <li
-          key={category.id}
-          className="relative group z-10 transition-all duration-200 hover:-translate-y-0.5"
-        >
-          <div className="absolute z-0 inset-px rounded-lg bg-white" />
+    <>
+      <ul className="grid max-w-6xl grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+        {categories.map((category) => (
+          <li
+            key={category.id}
+            className="relative group z-10 transition-all duration-200 hover:-translate-y-0.5"
+          >
+            <div className="absolute z-0 inset-px rounded-lg bg-white" />
 
-          <div className="pointer-events-none inset-px z-0 absolute rounded-lg shadow-sm transition-all duration-300 group-hover:shadow-md ring-1 ring-black/5" />
-          <div className="relative p-6 z-10">
-            <div className="flex items-center gap-4 mb-6">
-              <div
-                className="size-12 rounded-full"
-                style={{
-                  backgroundColor: category.color
-                    ? `#${category.color.toString(16).padStart(6, "0")}`
-                    : "#f3f4f6",
-                }}
-              />
+            <div className="pointer-events-none inset-px z-0 absolute rounded-lg shadow-sm transition-all duration-300 group-hover:shadow-md ring-1 ring-black/5" />
+            <div className="relative p-6 z-10">
+              <div className="flex items-center gap-4 mb-6">
+                <div
+                  className="size-12 rounded-full"
+                  style={{
+                    backgroundColor: category.color
+                      ? `#${category.color.toString(16).padStart(6, "0")}`
+                      : "#f3f4f6",
+                  }}
+                />
 
-              <div>
-                <h3 className="text-lg/7 font-medium tracking-tight text-gray-950">
-                  {category.emoji || "🗂️"} {category.name}
-                </h3>
-                <p className="text-sm/6 text-gray-600">
-                  {format(category.createdAt, "MMM d, yyyy")}
-                </p>
+                <div>
+                  <h3 className="text-lg/7 font-medium tracking-tight text-gray-950">
+                    {category.emoji || "🗂️"} {category.name}
+                  </h3>
+                  <p className="text-sm/6 text-gray-600">
+                    {format(category.createdAt, "MMM d, yyyy")}
+                  </p>
+                </div>
+              </div>
+
+              <div className="space-y-3 mb-6">
+                <div className="flex items-center text-sm/6 text-gray-600">
+                  <Clock className="size-4 mr-2 text-brand-500" />
+                  <span className="font-medium">Last ping:</span>
+                  <span className="ml-1">
+                    {category.lastPing
+                      ? formatDistanceToNow(category.lastPing) + "ago"
+                      : "Never"}
+                  </span>
+                </div>
+
+                <div className="flex items-center text-sm/6 text-gray-600">
+                  <Database className="size-4 mr-2 text-brand-500" />
+                  <span className="font-medium">Unique fields:</span>
+                  <span className="ml-1">{category.uniqueFieldCount || 0}</span>
+                </div>
+
+                <div className="flex items-center text-sm/6 text-gray-600">
+                  <BarChart2 className="size-4 mr-2 text-brand-500" />
+                  <span className="font-medium">Events this month:</span>
+                  <span className="ml-1">{category.eventsCount || 0}</span>
+                </div>
+              </div>
+
+              <div className="flex items-center justify-between mt-4">
+                <Link
+                  href={`/dashboard/category/${category.name}`}
+                  className={buttonVariants({
+                    variant: "outline",
+                    size: "sm",
+                    className: "flex items-center gap-2 text-sm",
+                  })}
+                >
+                  View all <ArrowRight className="size-4" />
+                </Link>
+                <Button
+                  variant={"ghost"}
+                  size={"sm"}
+                  className="text-gray-500 hover:text-red-600 transition-colors"
+                  aria-label={`Delete category ${category.name}`}
+                  onClick={() => setDeletingCategory(category.name)}
+                >
+                  <Trash2 className="size-5" />
+                </Button>
               </div>
             </div>
+          </li>
+        ))}
+      </ul>
 
-            <div className="space-y-3 mb-6">
-              <div className="flex items-center text-sm/6 text-gray-600">
-                <Clock className="size-4 mr-2 text-brand-500" />
-                <span className="font-medium">Last ping:</span>
-                <span className="ml-1">
-                  {category.lastPing
-                    ? formatDistanceToNow(category.lastPing) + "ago"
-                    : "Never"}
-                </span>
-              </div>
-
-              <div className="flex items-center text-sm/6 text-gray-600">
-                <Database className="size-4 mr-2 text-brand-500" />
-                <span className="font-medium">Unique fields:</span>
-                <span className="ml-1">{category.uniqueFieldCount || 0}</span>
-              </div>
-
-              <div className="flex items-center text-sm/6 text-gray-600">
-                <BarChart2 className="size-4 mr-2 text-brand-500" />
-                <span className="font-medium">Events this month:</span>
-                <span className="ml-1">{category.eventsCount || 0}</span>
-              </div>
-            </div>
+      <Modal
+        showModal={!!deletingCategory}
+        className="max-w-md p-8"
+        setShowModal={() => {
+          setDeletingCategory(null)
+        }}
+      >
+        <div className="space-y-6">
+          <div>
+            <h2 className="text-lg/7 font-medium tracking-tight text-gray-950">
+              Delete Category
+            </h2>
+            <p className="text-sm/6 text-gray-600">
+              Are you sure you want to delete the category{" "}
+              <strong>{deletingCategory}</strong>?
+            </p>
           </div>
-        </li>
-      ))}
-    </ul>
+
+          <div className="flex justify-end space-x-3 pt-4 border-t">
+            <Button
+              variant={"outline"}
+              onClick={() => setDeletingCategory(null)}
+            >
+              Cancel
+            </Button>
+
+            <Button
+              disabled={isDeleteLoading}
+              variant={"destructive"}
+              onClick={() => {
+                deletingCategory && deleteCategory(deletingCategory)
+              }}
+            >
+              {isDeleteLoading ? (
+                <LoadingSpinner className="size-2 bg-red-500" />
+              ) : null}{" "}
+              Delete
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </>
   )
 }
