0b7e76f76ec46966d44b7c43107ab7090d02e336
Sasha Mihalache <sasha.mihalache@gmail.com>
2025-03-07 21:07:58 +0200

    feat: sync clerk user with prisma user

--- src/server/procedures.ts
+++ src/server/procedures.ts
@@ -1,32 +1,38 @@
+import { db } from "@/db"
 import { j } from "./__internals/j"
+import { currentUser } from "@clerk/nextjs/server"
+import { HTTPException } from "hono/http-exception"
 
-/**
- * Middleware for providing a built-in cache with your Prisma database.
- *
- * You can remove this if you don't like it, but caching can massively speed up your database queries.
- */
+const authMiddleware = j.middleware(async ({ c, next }) => {
+  return next({})
+  // const authHeader = c.req.header("Authorization")
 
-// const extendedDatabaseMiddleware = j.middleware(async ({ c, next }) => {
-//   const variables = env(c)
+  // if (authHeader) {
+  //   const apiKey = authHeader.split(" ")[1] // bearer <API_KEY>
 
-//   const pool = new Pool({
-//     connectionString: variables.DATABASE_URL,
-//   })
+  //   const user = await db.user.findUnique({
+  //     where: { apiKey },
+  //   })
 
-//   const adapter = new PrismaNeon(pool)
+  //   if (user) return next({ user })
+  // }
 
-//   const redis = new Redis({
-//     token: variables.REDIS_TOKEN,
-//     url: variables.REDIS_URL,
-//   })
+  // const auth = await currentUser()
 
-//   const db = new PrismaClient({
-//     adapter,
-//   }).$extends(cacheExtension({ redis }))
+  // if (!auth) {
+  //   throw new HTTPException(401, { message: "Unauthorized" })
+  // }
 
-//   // Whatever you put inside of `next` is accessible to all following middlewares
-//   return await next({ db })
-// })
+  // const user = await db.user.findUnique({
+  //   where: { externalId: auth.id },
+  // })
+
+  // if (!user) {
+  //   throw new HTTPException(401, { message: "Unauthorized" })
+  // }
+
+  // return next({ user })
+})
 
 /**
  * Public (unauthenticated) procedures
@@ -35,3 +41,4 @@ import { j } from "./__internals/j"
  */
 export const baseProcedure = j.procedure
 export const publicProcedure = baseProcedure
+export const privateProcedure = publicProcedure.use(authMiddleware)
